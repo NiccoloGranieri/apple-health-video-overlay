7865954343a72cae48ea37836e0f6f0e846b304d
Niccolò <granieriniccolo@gmail.com>
2023-03-19 16:28:40 +0100

    Functional HR overlay

--- frame-extraction.py
+++ frame-extraction.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 from blend_modes import difference
@@ -7,57 +8,95 @@ import json
 import datetime
 import ffmpeg # Video Metadata
 
-videoPath = sys.path[0] + "/video.mov"
+parent = os.getcwd()
+
+heart = cv2.imread("heart.png")
+
+for file in os.listdir(parent):
+    if file.endswith(".MOV"):
+      videoPath = os.path.join(parent, file)
+    if file.endswith(".json"):
+      workoutData = open(os.path.join(parent, file))
 
 jsonMetadata = ffmpeg.probe(videoPath)
 
-workoutData = open(sys.path[0] + "/2023-03-17_07-12-24_hk_1679033544.rungap.json")
+format = jsonMetadata["format"]
 
 for i in jsonMetadata["streams"]:
   if i.get("index") == 0:
     vidW = i.get("width")
     vidH = i.get("height")
-    vidFR = i.get("r_frame_rate")
-    vidLen = i.get("duration")
-    nested = i.get("tags")
-    vidCreationT = nested.get("creation_time")
-    usefulMeta = [vidW, vidH, vidFR, vidLen, vidCreationT]
-  
+    vidFR = float(i.get("r_frame_rate")[:-2])
+    vidLen = int(float(i.get("duration")))
+    nested = format["tags"]
+    vidCrT = nested.get("com.apple.quicktime.creationdate")[:-5]
+    adjVidCrT = datetime.datetime.strptime(vidCrT, '%Y-%m-%dT%H:%M:%S')
+    vidEndT = adjVidCrT + datetime.timedelta(seconds=vidLen)
+    usefulMeta = [vidW, vidH, vidFR, vidLen, adjVidCrT, vidEndT]
+   
 hr = []
 
 data = json.load(workoutData)
 
+min = "00:00:00"
+max = str(datetime.time(0, int(usefulMeta[3] / 60), usefulMeta[3] % 60))
+
 for i in data["laps"][0]["points"]:
     if i.get("hr"):
-        hr.append(i.get("hr"))
+        hrTime = datetime.datetime.fromtimestamp(i.get("time"))
+        delta = hrTime - usefulMeta[4]
+        try:
+          if datetime.datetime.strptime(str(delta), "%H:%M:%S") >= datetime.datetime.strptime(min, "%H:%M:%S"):
+            if datetime.datetime.strptime(str(delta), "%H:%M:%S") <= datetime.datetime.strptime(max, "%H:%M:%S"):
+              hr.append(i.get("hr"))
+        except:
+           pass
         # print(i.get("time"))
         # print(datetime.datetime.fromtimestamp(i.get("time")))
+
+hrFrameUpdate = (usefulMeta[2] * vidLen) / len(hr)
 
 vidcap = cv2.VideoCapture(videoPath)
 success,currentFrame = vidcap.read()
 count = 0
 
-
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-writer = cv2.VideoWriter(sys.path[0] + "/textvid.mov", fourcc, 60.0, (usefulMeta[1], usefulMeta[0]))
+writer = cv2.VideoWriter(sys.path[0] + "/overlayed.mov", fourcc, usefulMeta[2], (usefulMeta[1], usefulMeta[0]))
 
 img_array = []
 count = 0
 hrUpdate = 0
+textPadding = 10
 
+print("Processing video...")
 while success:
   font = cv2.FONT_HERSHEY_SIMPLEX
 
-  cv2.putText(currentFrame, str(hr[hrUpdate]), (50, 250), font, 3, (0, 0, 255), 2, cv2.LINE_4)
+  bg_color = (0,0,0)
+  bg = np.full((currentFrame.shape), bg_color, dtype=np.uint8)
 
-  writer.write(currentFrame)
+  cv2.putText(bg, str(hr[hrUpdate]), (50, 250), font, 3, (0, 0, 255), 2, cv2.LINE_4)
+
+  x,y,w,h = cv2.boundingRect(bg[:,:,2])
+
+  bg[y:y+66,x+w+5:x+w+71,:] = heart[0:66,0:66,:]
+
+  x -= textPadding
+  y -= textPadding
+  w += textPadding * 2 + 66
+  h += textPadding * 2
+  result = currentFrame.copy()
+  result[y:y+h, x:x+w] = bg[y:y+h, x:x+w]
+
+  writer.write(result)
+
+  # img3[100:400,100:400,:] = img2[100:400,100:400,:]
 
   success,currentFrame = vidcap.read()
-  print('Read a new frame: ', success)
 
   count += 1
 
-  if count % 285 == 0:
+  if count % int(hrFrameUpdate) == 0 and hrUpdate < len(hr) - 1:
     hrUpdate += 1
 
 writer.release()
