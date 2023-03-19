c2b6ef777e130026e1ddcd66a234feb3420c0b44
Niccolò <granieriniccolo@gmail.com>
2023-03-19 11:51:33 +0100

    Edited to work with RunGap data export

--- frame-extraction.py
+++ frame-extraction.py
@@ -3,22 +3,43 @@ import numpy as np
 from blend_modes import difference
 import sys
 import csv
+import json
+import datetime
+import ffmpeg # Video Metadata
 
+videoPath = sys.path[0] + "/video.mov"
+
+jsonMetadata = ffmpeg.probe(videoPath)
+
+workoutData = open(sys.path[0] + "/2023-03-17_07-12-24_hk_1679033544.rungap.json")
+
+for i in jsonMetadata["streams"]:
+  if i.get("index") == 0:
+    vidW = i.get("width")
+    vidH = i.get("height")
+    vidFR = i.get("r_frame_rate")
+    vidLen = i.get("duration")
+    nested = i.get("tags")
+    vidCreationT = nested.get("creation_time")
+    usefulMeta = [vidW, vidH, vidFR, vidLen, vidCreationT]
+  
 hr = []
 
-with open("VideoData.csv", 'r') as file:
-  csvreader = csv.reader(file, delimiter=';')
-  
-  for row in csvreader:
-    hr.append(row[2])
-    
+data = json.load(workoutData)
 
-vidcap = cv2.VideoCapture(sys.path[0] + "/video.mov")
+for i in data["laps"][0]["points"]:
+    if i.get("hr"):
+        hr.append(i.get("hr"))
+        # print(i.get("time"))
+        # print(datetime.datetime.fromtimestamp(i.get("time")))
+
+vidcap = cv2.VideoCapture(videoPath)
 success,currentFrame = vidcap.read()
 count = 0
 
-fourcc = cv2.VideoWriter_fourcc(*"MP4V")
-writer = cv2.VideoWriter(sys.path[0] + "/textvid.mov", fourcc, 60.0, (1080, 1920))
+
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+writer = cv2.VideoWriter(sys.path[0] + "/textvid.mov", fourcc, 60.0, (usefulMeta[1], usefulMeta[0]))
 
 img_array = []
 count = 0
@@ -27,7 +48,7 @@ hrUpdate = 0
 while success:
   font = cv2.FONT_HERSHEY_SIMPLEX
 
-  cv2.putText(currentFrame, hr[hrUpdate] + "<3", (250, 50), font, 3, (0, 0, 255), 2, cv2.LINE_4)
+  cv2.putText(currentFrame, str(hr[hrUpdate]), (50, 250), font, 3, (0, 0, 255), 2, cv2.LINE_4)
 
   writer.write(currentFrame)
 
